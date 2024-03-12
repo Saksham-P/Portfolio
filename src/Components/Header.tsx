5d8d18f4e83bd02a948e2c9ae9e661cf1f5c9314
Saksham Puri <84263133+Saksham-P@users.noreply.github.com>
2024-03-11 20:58:55 -0600

    Basic Landing Page with Effects Complete

---
+++ src/Components/Header.tsx
@@ -0,0 +1,36 @@
+import '../Styles/Header.css'
+
+
+const switchTheme = () => {
+  const rootElem = document.documentElement;
+  let dataTheme = rootElem.getAttribute('data-theme'), newTheme;
+  newTheme = (dataTheme === 'light') ? 'dark' : 'light';
+  rootElem.setAttribute('data-theme', newTheme);
+}
+
+function Header() {
+  return (
+    <>
+      <div id="header_background">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="14 -22 158 158">
+          <path d="M 40 2 c -10.2 2.1 -20 10 -23.4 18.8 c -2.6 7.1 -2.3 17.6 0.8 23.7 c 4.9 9.4 11.8 13.3 31.7 17.9
+           c 16.2 3.6 22.4 6.7 25 12 c 1.8 3.9 2.1 12.7 0.5 15.9 c -6.9 13.5 -29.7 15.5 -53.2 4.8 l -6.7 -3 v 7.5 c 
+           0 8.6 -0.7 8 13.5 11.5 c 25.7 6.3 49.4 1.7 58.2 -11.3 c 6.3 -9.5 6.6 -25 0.5 -34.7 c -5.1 -8.3 -15.7 -13.4 
+           -36.7 -17.6 c -15.7 -3.2 -20.5 -7.2 -20.5 -17.1 c 0 -6.7 2.9 -11.1 9.5 -14.4 c 1.667 -0.833 4.9 -2.5 10.7 
+           -2.5 l 41.8 -0.3 c 32.1 -0.2 48.7 0 51.2 0.8 c 10.2 3 15.2 12.8 12.7 24.8 c -0.9 4.5 -2 6.5 -4.9 9.3 c 
+           -4.8 4.6 -10 5.9 -23.4 5.9 h -10.6 v -27 h -15 v 87 h 15 v -47.8 l 15.3 -0.4 c 12.8 -0.4 16.1 -0.8 21 
+           -2.7 c 12.2 -4.8 18.7 -15.1 18.7 -29.5 c 0 -14.8 -5.3 -23.7 -17.3 -29.2 c -2.067 -0.967 -6.1 -2.9 -10.7 
+           -2.9 l -52 -0.2 c -28.6 -0.1 -49.2 0.2 -50.3 0.4 z" fill='var(--logo-fill)'/>
+        </svg>
+        <div id='header_link_section'>
+          <a href="/#header" className="header-links">Projects</a>
+          <a href="/#header" className="header-links">My Journey</a>
+          <a href="/#header" className="header-links">Contact</a>
+          <button id='header_theme_button' onClick={switchTheme}></button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header
---
+++ src/Pages/LandingPage.tsx
@@ -0,0 +1,67 @@
+import '../Styles/LandingPage.css'
+import portrait from '../Assets/portrait.png'
+import codeImage from '../Assets/codeImageLight.png'
+
+function LandingPage() {
+  return (
+    <>
+      <div id="intro_section">
+        <div id="intro_text">
+          <p>Hi! I am</p>
+          <h1>Saksham Puri</h1>
+          <div id='intro_links'>
+            <a href='https://www.linkedin.com/in/sakshamp7'>LinkedIn</a>
+            <a href='https://github.com/saksham-P'>GitHub</a>
+          </div>
+        </div>
+        <div id='intro_img_container'>
+          <img id='intro_img' src={portrait} alt='portrait'></img>
+        </div>
+      </div>
+      <div id="intro_section2">
+        <div id="intro_text">
+          <p>Hi! I am</p>
+          <h1>Saksham Puri</h1>
+          <div id='intro_links'>
+            <a href='https://www.linkedin.com/in/sakshamp7'>LinkedIn</a>
+            <a href='https://github.com/saksham-P'>GitHub</a>
+          </div>
+        </div>
+        <img id='intro_img' src={portrait} alt='portrait'></img>
+      </div>
+      <div id='about_section'>
+        <div id='about_left_section'>
+          <h1>About Me</h1>
+          <div id='about_left_text'>
+            <p>I am a Software Engineer and an adventure seeker. I am a 3rd year Computer Science student
+              at University of Calgary, and a Co-op AI developer Student at City of Calgary. I am interested in
+              all things tech related, and mostly passionate about Machine Learning and it's applications
+              specifically within Computer Vision.</p>
+          </div>
+          <h1 className='hidden'>About Me</h1>
+          <div id='about_left_text2'>
+            <p>I am a Software Engineer and an adventure seeker. I am a 3rd year Computer Science student
+              at University of Calgary, and a Co-op AI developer Student at City of Calgary. I am interested in
+              all things tech related, and mostly passionate about Machine Learning and it's applications
+              specifically within Computer Vision.</p>
+          </div>
+        </div>
+        <div id="intro_section3">
+          <div id="intro_text" >
+            <p>Hi! I am</p>
+            <h1>Saksham Puri</h1>
+            <div id='intro_links'>
+              <a href='https://www.linkedin.com/in/sakshamp7'>LinkedIn</a>
+              <a href='https://github.com/saksham-P'>GitHub</a>
+            </div>
+          </div>
+          <div id='intro_img_container'>
+            <img id='intro_img3' src={codeImage} alt='codingIcons'></img>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default LandingPage
