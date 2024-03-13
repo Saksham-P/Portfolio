7cd053fbfb4d977eef320cb2851c702f635cc85d
Saksham Puri <84263133+Saksham-P@users.noreply.github.com>
2024-03-12 20:59:55 -0600

    Completed Contact Section frontend

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,7 +1,41 @@
+import '../Styles/Contact.css'
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <>
+      <div id="contact_background">
+        <div id='contact_link_section'>
+          <div className='contact_links'>
+            <h1>Explore My Projects</h1>
+            <a href='/projects#passion'>Passion Projects</a>
+            <a href='/projects#hackathon'>Hackathon Projects</a>
+            <a href='/projects#course'>Course Projects</a>
+          </div>
+          <div className='contact_links'>
+            <h1>My Journey</h1>
+            <a href='/journey#experience'>Experience</a>
+            <a href='/journey#education'>Education</a>
+          </div>
+          <div className='contact_links'>
+            <h1>Find Me</h1>
+            <a href='https://www.linkedin.com/in/sakshamp7'>LinkedIn</a>
+            <a href='https://github.com/saksham-P'>GitHub</a>
+          </div>
+        </div>
+        <div id='contact_me_section'>
+          <h1>Get In Touch</h1>
+          <form>
+            <div id='contact_me_input'>
+              <input className="input" type="text" name="name" placeholder="Name" />
+              <input className="input" type="text" name="email" placeholder="Email Adress" />
+            </div>
+            <textarea id="contact_message" className="input" name="message" placeholder="Your Message"></textarea>
+            <button id="contact_button" type="button">Send</button>
+          </form>
+        </div>
+      </div>
+      <h1 id='contact_footer'>© Made with &lt;/&gt; by Saksham. 2024</h1>
+    </>
   )
 }
 
--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -1,12 +1,62 @@
 import '../Styles/LandingPage.css'
 import portrait from '../Assets/portfolio.png'
 import codeImage from '../Assets/codeImageLight.png'
+import Contact from '../Components/Contact'
+
+window.onscroll = function () {
+  const aboutSection = document.getElementById('about_section');
+
+  if (aboutSection != null) {
+    let top = aboutSection.getBoundingClientRect().top;
+
+    if (top < 0) {
+      let el1 = document.getElementById('about_left_section');
+      let el2 = document.getElementById('about_left_text');
+      let el3 = document.getElementById('code_img_section');
+
+      if (el1 != null) {
+        el1.classList.remove('about_left_section_fixed');
+        el1.classList.add('about_left_section_fixed_removed');
+      }
+
+      if (el2 != null) {
+        el2.classList.remove('fixed');
+        el2.classList.add('absolute');
+      }
+
+      if (el3 != null) {
+        el3.classList.remove('fixed');
+        el3.classList.add('absolute');
+      }
+
+    } else {
+      let el1 = document.getElementById('about_left_section');
+      let el2 = document.getElementById('about_left_text');
+      let el3 = document.getElementById('code_img_section');
+
+      if (el1 != null) {
+        el1.classList.add('about_left_section_fixed');
+        el1.classList.remove('about_left_section_fixed_removed');
+      }
+
+      if (el2 != null) {
+        el2.classList.add('fixed');
+        el2.classList.remove('absolute');
+      }
+
+      if (el3 != null) {
+        el3.classList.add('fixed');
+        el3.classList.remove('absolute');
+      }
+    }
+  }
+};
 
 function LandingPage() {
   return (
     <>
       <div className='intro_container'>
-        <div className="intro_section">
+        <div className="intro_section fixed">
           <div className="intro_text">
             <p>Hi! I am</p>
             <h1>Saksham Puri</h1>
@@ -24,9 +74,9 @@ function LandingPage() {
       </div>
       <div id='about_section'>
         <div id='about_left_container'>
-          <div id='about_left_section'>
+          <div id='about_left_section' className='about_left_section_fixed'>
             <h1>About Me</h1>
-            <div id='about_left_text'>
+            <div id='about_left_text' className='fixed'>
               <p>I am a Software Engineer and an adventure seeker. I am a 3rd year Computer Science student
                 at University of Calgary, and a Co-op AI developer Student at City of Calgary. I am interested in
                 all things tech related, and mostly passionate about Machine Learning and it's applications
@@ -34,7 +84,7 @@ function LandingPage() {
             </div>
           </div>
         </div>
-        <div className="intro_section" style={{top: '10vh'}}>
+        <div id='code_img_section' className="intro_section fixed">
           <div className="intro_text hidden">
             <p>Hi! I am</p>
             <h1>Saksham Puri</h1>
@@ -50,6 +100,7 @@ function LandingPage() {
           </div>
         </div>
       </div>
+      <Contact />
     </>
   )
 }
